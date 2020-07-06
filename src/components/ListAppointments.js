da8c3aa8d0a955f5d7155da013ef3a7f4e357e6c
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-06 11:09:35 +0000

    Style the ListAppointment

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
 function ListAppointments({ appointments }) {
-  const listItems = appointments.map((item) => (
-    <div>
-      <div>{item.petName}</div>
-      <div>{item.ownerName}</div>
+  return (
+    <div className="appointment-list item-list mb-3">
+      {appointments.map((item) => (
+        <div className="pet-item col media py-3">
+          <div className="mr-3">
+            <button className="pet-delete btn btn-sm btn-danger">X</button>
+          </div>
+
+          <div className="pet-info media-body">
+            <div className="pet-head d-flex">
+              <span className="pet-name">{item.petName}</span>
+              <span className="apt-date ml-auto">{item.aptDate}</span>
+            </div>
+
+            <div className="owner-name">
+              <span className="label-item">Owner: </span>
+              <span>{item.ownerName}</span>
+            </div>
+            <div className="apt-notes">{item.aptNotes}</div>
+          </div>
+        </div>
+      ))}
     </div>
-  ));
-  return <div>{listItems}</div>;
+  );
 }
 
 export default ListAppointments;
