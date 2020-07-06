84a32caef2d577253ac140b129f96fe4c1813ec2
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-06 10:46:01 +0000

    Add data.json to the public file

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
 
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myName: 'Mouhamadou',
+    };
+  }
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
