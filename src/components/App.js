a7f04b5a757fc5239294bdbc5fa230409fb9d3ab
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-07 09:31:45 +0000

    Add the toggleForm function to toggle the forms on click

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,13 @@ class App extends Component {
       formDisplay: false,
     };
     this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+  }
+
+  toggleForm() {
+    this.setState({
+      formDisplay: !this.state.formDisplay,
+    });
   }
 
   deleteAppointment(apt) {
@@ -44,7 +51,10 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments formDisplay={this.state.formDisplay} />
+                <AddAppointments
+                  formDisplay={this.state.formDisplay}
+                  toggleForm={this.toggleForm}
+                />
                 <SearchAppointments />
                 <ListAppointments
                   appointments={this.state.myAppointments}
